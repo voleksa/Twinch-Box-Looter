dd04f33d91733d3b7ada1332a95329b0ec7ab4e6
Volodymyr_Oleksa <oleksavolodymyr@gmail.com>
2020-01-21 17:05:49 +0200

    refactoring

--- run.js
+++ run.js
@@ -1,15 +1,14 @@
 (function twitchboxlooter() {
-    setTimeout(function () {
-        setInterval(() => {
-            var balance = window.document.querySelector("p[data-test-selector='balance-string']");
-            console.log(balance.textContent);
-            var box = window.document.querySelector("div[class = 'claimable-bonus__icon tw-flex']");
-            if (box) {
-                box.click();
-                console.log("looted!");
-            }
 
-        }, 25000);
-    }, 5000)
+    setTimeout(() => {
+        var balance = window.document.querySelector("p[data-test-selector='balance-string']");
+        console.log(balance.textContent);
+        var box = window.document.querySelector("div[class = 'claimable-bonus__icon tw-flex']");
+        if (box) {
+            box.click();
+            console.log("looted!");
+        }
+        setTimeout(twitchboxlooter, 55000);
+    }, 5000);
 })()
 
